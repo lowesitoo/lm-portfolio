1d972c9559f34d0a7f71bef5950115d1a735b9a0
Antonio Luis A. Martinez <antonioluismartinez123@gmail.com>
2022-09-29 19:59:12 +0800

    feature: refactored script file

--- data.js
+++ data.js
@@ -1,9 +1,14 @@
 var h1InitialCount = 0;
-var  pInitialCount= 0;
-var h1Text = "Hey there! I'm Luis!";
+var h1InitialCount2 = 0;
+var pInitialCount= 0;
+var h1Text = "Hey there! ";
+var h1Text2 = "I'm Luis!"
 var pText = "A Full Stack Developer";
 var speed = 50;
 
+var dataList = [{
+  
+}]
 
 
 function typeWriter()  {
@@ -12,12 +17,14 @@ function typeWriter()  {
     h1InitialCount++;
     setTimeout(typeWriter, speed);
   }
+  else if (h1InitialCount2 < h1Text2.length) {
+    document.getElementById("h1-typewriter2").innerHTML += h1Text2.charAt(h1InitialCount2);
+    h1InitialCount2++;
+    setTimeout(typeWriter, speed);
+  }
   else if (pInitialCount < pText.length) {
     document.getElementById("p-typewriter").innerHTML += pText.charAt(pInitialCount);
     pInitialCount++;
     setTimeout(typeWriter, speed);
-  }
-  else if (h1InitialCount == h1Text.length || pInitialCount == pText.length) {
-    document.getElementById("h1-typewriter").innerHTML
   }
 }
