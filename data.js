c037b227d29a472d10f1b70c196051707f96cbb2
Antonio Luis A. Martinez <luismgamedev@gmail.com>
2022-09-30 12:44:26 +0800

    feature: removed typewriter effect on name

--- data.js
+++ data.js
@@ -1,9 +1,5 @@
-var h1InitialCount = 0;
-var h1InitialCount2 = 0;
-var pInitialCount= 0;
-var h1Text = "Hey there! ";
-var h1Text2 = "I'm Luis!"
-var pText = "A Full Stack Developer";
+  var pInitialCount= 0;
+var pText = "Full Stack Developer";
 var speed = 50;
 
 var skillSetList = [{
@@ -15,18 +11,9 @@ var skillSetList = [{
     image : "CSS3.png"
 }
 ]
+
 function typeWriter()  {
-  if (h1InitialCount < h1Text.length) {
-    document.getElementById("h1-typewriter").innerHTML += h1Text.charAt(h1InitialCount);
-    h1InitialCount++;
-    setTimeout(typeWriter, speed);
-  }
-  else if (h1InitialCount2 < h1Text2.length) {
-    document.getElementById("h1-typewriter2").innerHTML += h1Text2.charAt(h1InitialCount2);
-    h1InitialCount2++;
-    setTimeout(typeWriter, speed);
-  }
-  else if (pInitialCount < pText.length) {
+  if (pInitialCount < pText.length) {
     document.getElementById("p-typewriter").innerHTML += pText.charAt(pInitialCount);
     pInitialCount++;
     setTimeout(typeWriter, speed);
