5a48c9701fd4273025f953ae67a6dfbe712d7878
Antonio Luis A. Martinez <luismgamedev@gmail.com>
2022-10-03 23:53:36 +0800

    feature: added blinking cursor and removed unused lines

--- js/data.js
+++ js/data.js
@@ -1,6 +1,5 @@
 var pInitialCount = 0;
 var pText = "Full Stack Developer";
-var speed = 50;
 var sectionList = ['s1','s2','s3','s4']
 var skillSetList = [{
     skill : "HTML5",
@@ -18,23 +17,19 @@ function typeWriter()  {
     setTimeout(typeWriter, speed);
   }
 }
-function randomColor() {
-  var color = '#';
-  for (var i = 0; i < 6; i++) {
-      color += Math.floor(Math.random() * 10);
-  }
-  return color;
-  
-    // return "#" + Math.floor(Math.random() * 16777215).toString(16);
-}
-function changeColor() {
-  for (let i = 0; i < sectionList.length; i++) {
-    let currentSection = document.getElementById(sectionList[i]);
-    currentSection.style.backgroundColor = randomColor();
-  }
-}
 
-
+// Blinking Cursor
+var cursor = true;
+var speed = 250;
+  setInterval(() => {
+    if(cursor) {
+      document.getElementById('cursor').style.opacity = 0;
+      cursor = false;
+    }else {
+      document.getElementById('cursor').style.opacity = 1;
+      cursor = true;
+    }
+  }, speed);
 // Blur on scroll
 // $(document).ready(function() {
 //   $(window).scroll(function(e) {
