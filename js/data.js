71d33b3dbe81aac395d23ab87426791f2c00f07b
Antonio Luis A. Martinez <luismgamedev@gmail.com>
2022-10-04 18:30:23 +0800

    feature: added cursor blink

--- js/data.js
+++ js/data.js
@@ -20,17 +20,17 @@ function typeWriter()  {
 }
 
 // Blinking Cursor
-// var cursor = true;
+var cursor = true;
 // var speed = 250;
-//   setInterval(() => {
-//     if(cursor) {
-//       document.getElementById('cursor').style.opacity = 0;
-//       cursor = false;
-//     }else {
-//       document.getElementById('cursor').style.opacity = 1;
-//       cursor = true;
-//     }
-//   }, speed);
+  setInterval(() => {
+    if(cursor) {
+      document.getElementById('cursor').style.opacity = 0;
+      cursor = false;
+    }else {
+      document.getElementById('cursor').style.opacity = 1;
+      cursor = true;
+    }
+  }, speed);
 
 // Blur on scroll
 // $(document).ready(function() {
